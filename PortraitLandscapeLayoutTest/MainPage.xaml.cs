a16f7f18887d74ac074154e6b161f10bc415dd0a
RussellTheCodeMonkey <166459325+RussellTheCodeMonkey@users.noreply.github.com>
2024-07-03 02:47:55 +1000

    Add project files.

---
+++ PortraitLandscapeLayoutTest/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace PortraitLandscapeLayoutTest;
+
+public partial class MainPage : ContentPage {
+    public MainPage() {
+        InitializeComponent();
+            
+        // Add our function to the SizeChanged event
+        SizeChanged += (sender, e) => ApplyDynamicGrid(sender, e, "MainGrid");
+    }
+
+    /// <summary>
+    /// Process the SizeChanged event and apply the appropriate VisualState to the grid
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <param name="OurGrid">Name of grid to process (so we don't have to search for it)</param>
+    private void ApplyDynamicGrid(object? sender, EventArgs e, string OurGrid) {
+        var GridName = this.FindByName<Grid>(OurGrid);
+
+        Console.WriteLine("SizeChanged event triggered");
+
+        if (Width > Height) {
+            Console.WriteLine("Switching to Landscape mode");
+            MyVisualStateManagerGridHandling.ApplyVisualState(GridName, "Landscape");
+        } else {
+            Console.WriteLine("Switching to Portrait mode");
+            MyVisualStateManagerGridHandling.ApplyVisualState(GridName, "Portrait");
+        }
+    }
+}
---
+++ PortraitLandscapeLayoutTest/MyVisualStateManagerGridHandling.cs
@@ -0,0 +1,37 @@
+﻿namespace PortraitLandscapeLayoutTest;
+
+public static class MyVisualStateManagerGridHandling {
+    /// <summary>
+    /// Iterate through the VisualStateGroups and apply the appropriate VisualState to the grid
+    /// </summary>
+    /// <param name="view"></param>
+    /// <param name="stateName">The name of the Grid we're working on</param>
+    public static void ApplyVisualState(BindableObject view, string stateName) {
+        if (view is VisualElement visualElement) {
+            var visualStateGroups = VisualStateManager.GetVisualStateGroups(visualElement);
+            foreach (var group in visualStateGroups) {
+                var state = group.States.FirstOrDefault(s => s.Name == stateName);
+                if (state != null) {
+                    foreach (var setter in state.Setters) {
+                        ApplySetter(visualElement, setter);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Apply the setter to the target element
+    /// </summary>
+    /// <param name="target">VisualElement</param>
+    /// <param name="setter">Setter</param>
+    private static void ApplySetter(VisualElement target, Setter setter) {
+        var targetElement = target.FindByName<Element>(setter.TargetName);
+        if (targetElement != null) {
+            //Console.WriteLine($"Applying setter: {setter.TargetName}.{setter.Property.PropertyName} = {setter.Value}");
+            targetElement.SetValue(setter.Property, setter.Value);
+        } else {
+            //Console.WriteLine($"Target element not found: {setter.TargetName}");
+        }
+    }
+}
