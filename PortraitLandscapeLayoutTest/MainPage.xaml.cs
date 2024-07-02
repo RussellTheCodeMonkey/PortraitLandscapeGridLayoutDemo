980ce049ea2be915665d1007f3c0ba1f1c86f6e4
RussellTheCodeMonkey <166459325+RussellTheCodeMonkey@users.noreply.github.com>
2024-07-03 02:50:11 +1000

    Added the code to modify the Grid layout manually, for your learning pleasure

--- PortraitLandscapeLayoutTest/MainPage.xaml.cs
+++ PortraitLandscapeLayoutTest/MainPage.xaml.cs
@@ -27,4 +27,64 @@ public partial class MainPage : ContentPage {
             MyVisualStateManagerGridHandling.ApplyVisualState(GridName, "Portrait");
         }
     }
+
+    // Handle orientation changes manually (ignoring xaml VisualStateManager)
+    /*
+    private void OnSizeChanged(object? sender, EventArgs e) {
+        if (Width > Height) {
+            // Landscape
+            MainGrid.RowDefinitions.Clear();
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            MainGrid.ColumnDefinitions.Clear();
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            Grid.SetRow(Box1, 0);
+            Grid.SetColumn(Box1, 0);
+            Grid.SetRowSpan(Box1, 1);
+            Grid.SetColumnSpan(Box1, 1);
+
+            Grid.SetRow(Box2, 0);
+            Grid.SetColumn(Box2, 1);
+            Grid.SetRowSpan(Box2, 2);
+            Grid.SetColumnSpan(Box2, 1);
+
+            Grid.SetRow(Box3, 1);
+            Grid.SetColumn(Box3, 0);
+            Grid.SetRowSpan(Box3, 1);
+            Grid.SetColumnSpan(Box3, 1);
+        } else {
+            // Portrait
+            MainGrid.RowDefinitions.Clear();
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            MainGrid.ColumnDefinitions.Clear();
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            Grid.SetRow(Box1, 0);
+            Grid.SetColumn(Box1, 0);
+            Grid.SetRowSpan(Box1, 1);
+            Grid.SetColumnSpan(Box1, 1);
+
+            Grid.SetRow(Box2, 1);
+            Grid.SetColumn(Box2, 0);
+            Grid.SetRowSpan(Box2, 1);
+            Grid.SetColumnSpan(Box2, 1);
+
+            Grid.SetRow(Box3, 2);
+            Grid.SetColumn(Box3, 0);
+            Grid.SetRowSpan(Box3, 1);
+            Grid.SetColumnSpan(Box3, 1);
+        }
+
+        // Log for debugging
+        Console.WriteLine($"Orientation changed to: {(Width > Height ? "Landscape" : "Portrait")}");
+    }
+}
+*/
+
 }
